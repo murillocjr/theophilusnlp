e12b72cb20f9e792a2f52ab951d88aa4c3b55a74
Ubuntu <ubuntu@ip-172-31-42-209.sa-east-1.compute.internal>
2020-07-29 05:59:53 +0000

    books-collected

--- retrieve-bible-books.py
+++ retrieve-bible-books.py
@@ -1,5 +1,7 @@
 import requests
 import parameters
+import json
+from tinydb import TinyDB, Query
 
 bible_id = parameters.BIBLE_ID
 url = 'https://api.scripture.api.bible/v1/bibles/' + bible_id + '/books'
@@ -7,4 +9,15 @@ url = 'https://api.scripture.api.bible/v1/bibles/' + bible_id + '/books'
 headers = {'api-key': parameters.API_KEY}
 r = requests.get(url, headers=headers)
 
-print(r.text)
+#print(r.text)
+books_dict = json.loads(r.text)
+
+db = TinyDB('db_'+parameters.BIBLE_ID+'.json')
+db.truncate()
+
+for book in books_dict['data']:
+    db.insert(book)
+
+print(len(db))
+
+
