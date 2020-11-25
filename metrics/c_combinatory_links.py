e9c15dab160b34678fa5577f843df7664ae40697
Jorge-Murillo <murillo.cjr@gmail.com>
2020-11-25 07:31:51 +0000

    1 link minumum coherency

--- metrics/c_combinatory_links.py
+++ metrics/c_combinatory_links.py
@@ -27,28 +27,53 @@ obj = json.loads(line)
 # print(obj)
 
 for i in range(0, len(list)):
-    for j in range(i+1, len(list)):
+    minDis = 2
+    k = i
+    # maxDis = 0
+    # m = i
+    for j in range(0, len(list)):
+        if i == j:
+            continue
+
         lda_fst =  models.LdaModel.load('../models/'+list[i]+'_model5.gensim')
         lda_snd =  models.LdaModel.load('../models/'+list[j]+'_model5.gensim')
 
         mdiff, annotation = lda_fst.diff(lda_snd, distance='jaccard', num_words=50)
 
+        #####
         minimum = 2
         for row in mdiff:
             for val in row:
-                if val>0:
+                if val > 0:
                     if val < minimum:
                         minimum = val
                     
-        if minimum == 2:
-            distance = 10
-        else:
-            distance = round(5000*(1-minimum))
+        if minimum < minDis:
+            k = j 
+            distanceMin = round(500*(minimum))
+            minDis = minimum
 
-        link = {'source': list[i], 'dest': list[j], 'distance': distance}
+        #####
+        # maximum = 0
+        # for row in mdiff:
+        #     for val in row:
+        #         if val < 1:
+        #             if val > maximum:
+        #                 maximum = val
+                    
+        # if maximum > maxDis:
+        #     m = j 
+        #     distanceMax = round(500*(maximum))
+        #     maxDis = maximum
 
-        obj['links'].append(link)
-        linkCount += 1
+    linkMin = {'source': list[i], 'dest': list[k], 'distance': distanceMin}
+    obj['links'].append(linkMin)
+    linkCount += 1
+
+    # linkMax = {'source': list[i], 'dest': list[m], 'distance': distanceMax}
+    # obj['links'].append(linkMax)
+    # linkCount += 1
+    
 
 obj['links_count'] = linkCount
 
