bcc4f1e6a53540640126459e88adae68c7d2236a
Jorge-Murillo <murillo.cjr@gmail.com>
2020-12-02 00:23:14 +0000

    using tika for pdf parsing

--- pdf/a_process_pdf.py
+++ pdf/a_process_pdf.py
@@ -6,8 +6,9 @@ from english_words import getEnglishWords
 
 from md5_utils import md5
 
-import PyPDF2 
-import textract
+# import PyPDF2
+# import textract
+from tika import parser
 from nltk.tokenize import word_tokenize
 import gensim
 from gensim import corpora
@@ -17,25 +18,33 @@ import json
 
 
 def processPDF(pdfFilePath, stopWords, englishWords, numTopics, numWords, modelPasses, outputPath, isScanned):
-    pdfFileObj = open(pdfFilePath,'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    num_pages = pdfReader.numPages
+    allText = parser.from_file(pdfFilePath)
+    # print(allText)
+    paragList = allText['content'].split('\n\n')
+    # print(list)
+
+
+
+    # pdfFileObj = open(pdfFilePath,'rb')
+    # pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # num_pages = pdfReader.numPages
     count = 0
     #
     text_data = []
     non_english = []
-    while count < num_pages:
-        pageObj = pdfReader.getPage(count)
+    # while count < num_pages:
+    for text in paragList:
+        # pageObj = pdfReader.getPage(count)
         count +=1
-        text = ""
+        # text = ""
         
-        if (not isScanned):
-           text = pageObj.extractText()
-        else:
-           text = textract.process(fileurl, method='tesseract', language='eng')
+        # if (not isScanned):
+           # text = parser.from_file('sample.pdf') # pageObj.extractText()
+        # else:
+           # text = textract.process(fileurl, method='tesseract', language='eng')
 
-        encoded_string = text.encode("ascii", "ignore")
-        text = encoded_string.decode()
+        # encoded_string = text.encode("ascii", "ignore")
+        # text = encoded_string.decode()
     
         tokens = word_tokenize(text)
     
@@ -74,15 +83,18 @@ def processPDF(pdfFilePath, stopWords, englishWords, numTopics, numWords, modelP
     metadata = {}
     metadata['title'] = title
     metadata['isScanned'] = isScanned
-    metadata['pagesCount'] = num_pages
+    # metadata['pagesCount'] = num_pages
     metadata['md5'] = filename
     metaFile = open(outputPath+'/'+filename+'.metadata', "w")
     metaFile.write(json.dumps(metadata))
     metaFile.close()
 
-    print('Processed: '+title)    
+    print('Processed: '+filename+'::'+title)
 
 
-# stop_words = getStopWords('../commons/stop_words')
-# english_words = getEnglishWords('../commons/english_words')
-# processPDF('files/The Creation Account in Genesis 1:1-3 Part IV: The Theology of Genesis 1.pdf', stop_words, english_words, 20, 4, 15, '../models', False)
+stop_words = getStopWords('../commons/stop_words')
+english_words = getEnglishWords('../commons/english_words')
+# file = 'files/Python 3 Text Processing with NLTK 3 Cookbook.pdf'
+# file = 'files/The Creation Account in Genesis 1:1-3 Part IV: The Theology of Genesis 1.pdf'
+file = 'files/6 Lessons Learned From Hundreds of Mobile Development Projects - Business Intelligence Info.pdf'
+processPDF(file, stop_words, english_words, 20, 4, 15, '../models', False)
