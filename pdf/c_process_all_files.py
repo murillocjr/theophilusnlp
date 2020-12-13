71898032ac5f471ca1ae4cd1e1375240c24cc43e
Jorge-Murillo <murillo.cjr@gmail.com>
2020-12-13 05:24:28 +0000

    processing files from Calibre Library, recursive, handling scanned-no-text-errors gracefully

--- pdf/c_process_all_files.py
+++ pdf/c_process_all_files.py
@@ -1,30 +1,29 @@
-from os import listdir
-from os.path import isfile, join
-from a_process_pdf import processPDF
 import process_parameters as pp
 import hashlib
 import os
-
 import sys
+from os.path import isfile, join
+from a_process_pdf import processPDF
+from pathlib import Path
 sys.path.append('../commons/')
 from stop_words import getStopWords
 from english_words import getEnglishWords
-
 from md5_utils import md5
 
-keepExisting = True
-folderName = 'files'
 stopWords = getStopWords('../commons/stop_words')
 englishWords = getEnglishWords('../commons/english_words')
 
 def filesList():
-    return [f for f in listdir(folderName) if isfile(join(folderName, f))]
-    
+    return list(Path(pp.PDF_FILES_PATH).rglob("*.[pP][dD][fF]"))
+        
 for file in filesList():
-    metadataPath = '../models/'+md5(folderName+'/'+file)+'.metadata'
-    if os.path.exists(metadataPath) and keepExisting:
-        print('Already Processed: '+file)
+    metadataPath = pp.MODEL_FILE_PATH+'/'+md5(file)+'.metadata'
+    if os.path.exists(metadataPath) and pp.KEEP_EXISTING_MODELS:
+        print('Already Processed: '+file.stem)
         continue
-    print('Start processing: '+file)
-    processPDF(folderName+'/'+file, stopWords, englishWords, pp.TOPICS_NUMBER, pp.WORDS_NUMBER, pp.PASSES_COUNT, '../models', False)
+    print('Start processing: '+file.stem)
+    try:
+        processPDF(file, stopWords, englishWords, pp.TOPICS_NUMBER, pp.WORDS_NUMBER, pp.PASSES_COUNT, pp.MODEL_FILE_PATH, False)
+    except:
+        print('Error Processing: '+file.stem)
     #break
--- pdf/process_parameters.py
+++ pdf/process_parameters.py
@@ -1,3 +1,6 @@
 TOPICS_NUMBER = 20
 WORDS_NUMBER = 4
 PASSES_COUNT = 15
+PDF_FILES_PATH = '/home/ubuntu/Calibre Library'
+MODEL_FILE_PATH = '/home/ubuntu/theophilusnlp/models'
+KEEP_EXISTING_MODELS = True
